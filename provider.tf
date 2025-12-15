23f9c0186be2dc63d2e55dfe5f912c394bba0404
Muhammad safad <safadchappuek@gmail.com>
2025-12-15 20:39:25 +0530

    Rename S3 bucket for Terraform state

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   # Remote backend to store Terraform state
   backend "s3" {
-    bucket = "my-terraform-state-bucket-my-12345"   # ⚠️ replace with YOUR bucket name
+    bucket = "terraform-state-bucket-my-12345"   # ⚠️ replace with YOUR bucket name
     key    = "terraform/github/terraform.tfstate"
     region = "ap-south-1"
   }
