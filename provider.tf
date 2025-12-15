2443da9b54851d561d5b9384746070514ccd3b26
Muhammad safad <safadchappuek@gmail.com>
2025-12-15 19:03:14 +0530

    Add AWS provider configuration in provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+# This block tells Terraform which cloud provider to use
+provider "aws" {
+
+  # AWS region where resources will be created
+  # ap-south-1 = Mumbai region
+  region = "ap-south-1"
+}
