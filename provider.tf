43e09b94904a7e243d8c4a48dd8acfe1106a03e8
Muhammad safad <safadchappuek@gmail.com>
2025-12-15 20:32:49 +0530

    Configure S3 backend for Terraform state
    Added remote backend configuration for Terraform state storage in S3.

--- provider.tf
+++ provider.tf
@@ -1,7 +1,13 @@
-# This block tells Terraform which cloud provider to use
-provider "aws" {
+terraform {
+  # Remote backend to store Terraform state
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-my-12345"   # ⚠️ replace with YOUR bucket name
+    key    = "terraform/github/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
 
-  # AWS region where resources will be created
-  # ap-south-1 = Mumbai region
+# AWS provider configuration
+provider "aws" {
   region = "ap-south-1"
 }
