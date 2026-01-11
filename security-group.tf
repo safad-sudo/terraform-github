38ab359ce715d35efa10c64e33be28a2408f34c0
Muhammad safad <safadchappuek@gmail.com>
2026-01-11 12:33:19 +0530

    Add HTTP ingress rule to security group

--- security-group.tf
+++ security-group.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ ingress {
+    description = "http (optional)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
